9550cfcaf64fc249651e74ebf74960f5349866ec
Danielle Mujica <danielle.mujica@wsu.edu>
2021-04-19 09:41:59 -0700

    setting up project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="WSU School of Medicine logo" class="logo" src="./assets/color-logo.png">
+    <Home />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
@@ -21,8 +21,12 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+  padding: 40px 80px;
+}
+.logo {
+  max-width: 267px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>STYLE INVENTORY</h1>
+    <h2>Instructions</h2>
+    <h3>Understanding Your Style</h3>
+    <ol>
+      <li>Complete the Personal Style Inventory. Review the results indicated by the longest bar.
+        <ul>
+          <li>Do you perceive it as accurate?</li>
+          <li>What does it mean for you as a member of this team?</li>
+          <li>Do you recognize these styles in the behavior of others?</li>
+        </ul>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
